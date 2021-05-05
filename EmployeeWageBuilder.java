c4fc63245ea997e2e79c77d5cda77b10d7ff44af
prudhvi <prudhvieethakota123@gmail.com>
2021-05-05 09:06:19 +0530

    Added:Welcome Message to Employee Wage Program

---
+++ EmployeeWageBuilder.java
@@ -0,0 +1,11 @@
+package employeewage;
+
+public class EmployeeWageBuilder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation Program");
+
+	}
+
+}
