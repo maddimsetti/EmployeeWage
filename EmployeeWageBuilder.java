13343cbefae94401792a522a585de5174dee27e8
prudhvi <prudhvieethakota123@gmail.com>
2021-05-05 12:09:09 +0530

    Added:Refactor the Code And Computed in Class Method

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,20 +1,38 @@
 package employeewage;
 
+//class Declaration
 public class EmployeeWageBuilder {
+
+	// constants
 	public static final int FULL_TIME = 1;
 	public static final int PART_TIME = 2;
-	public static final int WAGE_PER_HOUR = 20;
-	public static final int WORKING_DAYS_PER_MONTH = 20;
-	public static final int EMP_HOURS_IN_MONTH = 100;
+	public final int WAGE_PER_HOUR;
+	public final int WORKING_DAYS_PER_MONTH;
+	public final int EMP_HOURS_IN_MONTH;
+	// variable
+	public final String company;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Welcome to Employee Wage Computation Program");
+	// Constructor Declaration of class
+	public EmployeeWageBuilder(String company, int WAGE_PER_HOUR, int WORKING_DAYS_PER_MONTH, int EMP_HOURS_IN_MONTH) {
+		this.company = company;
+		this.WAGE_PER_HOUR = WAGE_PER_HOUR;
+		this.WORKING_DAYS_PER_MONTH = WORKING_DAYS_PER_MONTH;
+		this.EMP_HOURS_IN_MONTH = EMP_HOURS_IN_MONTH;
+	}
+
+	// method1
+	public int empCheck() {
+
+		return (int) Math.floor(Math.random() * 10) % 3;
+	}
+
+	// method2
+	public int totalEmpWorkingHours() {
 		int empWorkingHours = 0, totalEmpWorkingHours = 0, totalWorkingDays = 0;
 		while (totalEmpWorkingHours <= EMP_HOURS_IN_MONTH && totalWorkingDays < WORKING_DAYS_PER_MONTH) {
 			totalWorkingDays++;
-			int empCheck = (int) Math.floor(Math.random() * 100) % 3;
-			switch (empCheck) {
+
+			switch (empCheck()) {
 			case FULL_TIME:
 				empWorkingHours = 8;
 				break;
@@ -26,11 +44,21 @@ public class EmployeeWageBuilder {
 				break;
 			}
 			totalEmpWorkingHours += empWorkingHours;
-			System.out.println("Day# " + totalWorkingDays + "  EmpWorkingHours: " + empWorkingHours);
+			System.out.println("Day# " + totalWorkingDays + "  EmpWorkingHours:" + empWorkingHours);
 		}
-		int totalEmployeeWage = totalEmpWorkingHours * WAGE_PER_HOUR;
-		System.out.println("*****Total Salary for a Month:" + totalEmployeeWage);
-
+		return totalEmpWorkingHours;
 	}
 
+	// method3
+	public int totalEmployeeWage() {
+		return totalEmpWorkingHours() * WAGE_PER_HOUR;
+	}
+
+	// Compute
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Employee Wage Computation Program");
+		EmployeeWageBuilder dmart = new EmployeeWageBuilder("Dmart", 20, 20, 100);
+		System.out.println("Total Monthly Salary for " + dmart.company + " is " + dmart.totalEmployeeWage());
+	}
 }
