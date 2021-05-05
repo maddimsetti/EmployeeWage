38f2ffeba2b25643556e829012ddfeaad06acd41
prudhvi <prudhvieethakota123@gmail.com>
2021-05-05 09:35:21 +0530

    Added:Code for Adding PartTime Employee and Wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -6,12 +6,16 @@ public class EmployeeWageBuilder {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
 		int FULL_TIME = 1;
+		int PART_TIME = 2;
 		int WAGE_PER_HOUR = 20;
 		int empWorkingHours = 0;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 100) % 3;
 		if (empCheck == FULL_TIME) {
 			System.out.println("Employee is present");
 			empWorkingHours = 8;
+		} else if (empCheck == PART_TIME) {
+			System.out.println("Employee is Present for PartTime");
+			empWorkingHours = 4;
 		} else {
 			System.out.println("Employee is Absent");
 			empWorkingHours = 0;
