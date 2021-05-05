0f822d1af07ad08389314e39a12006863b9a9d7a
prudhvi <prudhvieethakota123@gmail.com>
2021-05-05 10:12:26 +0530

    Added:Code for Calculating EmployeeWage for a Month

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -4,25 +4,30 @@ public class EmployeeWageBuilder {
 	public static final int FULL_TIME = 1;
 	public static final int PART_TIME = 2;
 	public static final int WAGE_PER_HOUR = 20;
+	public static final int WORKING_DAYS_PER_MONTH = 20;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int empWorkingHours = 0;
-		int empCheck = (int) Math.floor(Math.random() * 100) % 3;
-		switch (empCheck) {
-		case FULL_TIME:
-			empWorkingHours = 8;
-			break;
-		case PART_TIME:
-			empWorkingHours = 4;
-			break;
-		default:
-			empWorkingHours = 0;
-			break;
+		int empWorkingHours = 0, total_Salary = 0;
+		for (int day = 1; day <= WORKING_DAYS_PER_MONTH; day++) {
+			int empCheck = (int) Math.floor(Math.random() * 100) % 3;
+			switch (empCheck) {
+			case FULL_TIME:
+				empWorkingHours = 8;
+				break;
+			case PART_TIME:
+				empWorkingHours = 4;
+				break;
+			default:
+				empWorkingHours = 0;
+				break;
+			}
+			int dailyEmployeeWage = WAGE_PER_HOUR * empWorkingHours;
+			System.out.println("Day " + day + " dailyEmployeeWage is " + dailyEmployeeWage);
+			total_Salary = total_Salary + dailyEmployeeWage;
 		}
-		int dailyEmployeeWage = WAGE_PER_HOUR * empWorkingHours;
-		System.out.println("dailyEmployeeWage:" + dailyEmployeeWage);
+		System.out.println("Total Salary for a Month:" + total_Salary);
 
 	}
 
