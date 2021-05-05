005d0621677de60c30f0c04655fe42b3769ff517
prudhvi <prudhvieethakota123@gmail.com>
2021-05-05 09:49:20 +0530

    Added:Solved the Employee Wage Using Switch Case Statement

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,24 +1,25 @@
 package employeewage;
 
 public class EmployeeWageBuilder {
+	public static final int FULL_TIME = 1;
+	public static final int PART_TIME = 2;
+	public static final int WAGE_PER_HOUR = 20;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int FULL_TIME = 1;
-		int PART_TIME = 2;
-		int WAGE_PER_HOUR = 20;
 		int empWorkingHours = 0;
-		double empCheck = Math.floor(Math.random() * 100) % 3;
-		if (empCheck == FULL_TIME) {
-			System.out.println("Employee is present");
+		int empCheck = (int) Math.floor(Math.random() * 100) % 3;
+		switch (empCheck) {
+		case FULL_TIME:
 			empWorkingHours = 8;
-		} else if (empCheck == PART_TIME) {
-			System.out.println("Employee is Present for PartTime");
+			break;
+		case PART_TIME:
 			empWorkingHours = 4;
-		} else {
-			System.out.println("Employee is Absent");
+			break;
+		default:
 			empWorkingHours = 0;
+			break;
 		}
 		int dailyEmployeeWage = WAGE_PER_HOUR * empWorkingHours;
 		System.out.println("dailyEmployeeWage:" + dailyEmployeeWage);
